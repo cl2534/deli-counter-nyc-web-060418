28f89c1c02ce66d82d4c713a384e5ed3f7a342d9
Changmin Lim <chang.lim92@gmail.com>
2018-05-26 04:18:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
 end 
 
 def now_serving(katz_deli)
-  katz_deli.length == 0 ? "There is nobody waiting to be served!" : 
+  katz_deli.length == 0 ? puts "There is nobody waiting to be served!" : 
   serve = katz_deli.shift 
   puts "Currently serving #{serve}."
 end 
