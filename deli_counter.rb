d9b1dd33eb7ecf28556f2667c8e505ab597dd3b6
Changmin Lim <chang.lim92@gmail.com>
2018-05-26 04:00:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+# Write your code here
+katz_deli = []
+
+def line(katz_deli)
+  if (katz_deli.size == 0) 
+    puts "The line is currently empty."
+  else 
+    current = ["The line is currently:"]
+    katz_deli.each_with_index { |i, index| 
+      current.push(" #{index+1}. #{i}") }
+    puts current.join("")
+  end 
+end 
+
+
+
+  
